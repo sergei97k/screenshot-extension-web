607ce6e9a451a7c500228d30090c51ea96e14622
Serhii Kononov <sergei97k@gmail.com>
2018-05-08 23:47:10 +0300

    integrate google drive api auth

--- src/App.js
+++ src/App.js
@@ -1,18 +1,101 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import constants from './constants';
+
 class App extends Component {
+  state = {
+    auth_token: null,
+    isSignedIn: false,
+    isLoad: false,
+  };
+
+  componentDidMount() {
+    const self = this;
+    const gapiScript = document.createElement('script');
+    gapiScript.src = 'https://apis.google.com/js/api.js?onload=onGapiLoad';
+    
+    window.onGapiLoad = function onGapiLoad() {
+      window.gapi.load('client:auth2', _ => self.initClient(self));
+    }
+
+    document.body.appendChild(gapiScript);
+  }
+
+  /**
+   *  Initializes the API client library and sets up sign-in state
+   *  listeners.
+   */
+  initClient(self) {
+    const { gapi } = window;
+    const { API_KEY, CLIENT_ID, DISCOVERY_DOCS, SCOPES } = constants;
+
+    gapi.client.init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES
+    }).then(_ => {
+      // Listen for sign-in state changes.
+      gapi.auth2.getAuthInstance().isSignedIn.listen(isSignedIn => this.updateSigninStatus(isSignedIn, self));
+
+      // Handle the initial sign-in state.
+      this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get(), self);
+
+      this.setState({
+        isLoad: true,
+        auth_token: gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token || null,
+      });
+    });
+  };
+
+  /**
+   *  Called when the signed in status changes, to update the UI
+   *  appropriately. After a sign-in, the API is called.
+   */
+  updateSigninStatus(isSignedIn, self) {
+    const { gapi } = window;
+    
+    self.setState({
+      isSignedIn,
+      auth_token: gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token || null,
+    });
+  }
+
+  /**
+   *  Sign in the user upon button click.
+   */
+  handleAuthClick(event) {
+    window.gapi.auth2.getAuthInstance().signIn();
+  }
+
+  /**
+   *  Sign out the user upon button click.
+   */
+  handleSignoutClick(event) {
+    window.gapi.auth2.getAuthInstance().signOut();
+  }
+
   render() {
+    const { isSignedIn, isLoad } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {!isLoad && (
+          <span>Loading...</span>
+        )}
+
+        {isLoad && (
+          !isSignedIn && (<button id="authorize-button" 
+            onClick={this.handleAuthClick}>Authorize</button>
+          )
+        )}
+
+        {isLoad && (
+          isSignedIn && (<button id="signout-button"
+            onClick={this.handleSignoutClick}>Sign Out</button>
+          )
+        )}
       </div>
     );
   }
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export default {
+  CLIENT_ID: '',
+  API_KEY: '',
+  DISCOVERY_DOCS: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
+  SCOPES: 'https://www.googleapis.com/auth/drive',
+};
